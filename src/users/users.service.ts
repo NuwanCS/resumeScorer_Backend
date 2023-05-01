d31327d8c624bb3530312e0f67d1d939aad3faa0
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2023-05-01 13:41:38 +0530

    created user service

---
+++ src/users/users.service.ts
@@ -0,0 +1,56 @@
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+  async createUser(email: string, password: string) {
+    const user = this.userRepository.create({ email, password });
+
+    const foundUser = await this.userRepository.find({
+      where: {
+        email,
+      },
+    });
+
+    if (foundUser) {
+      throw new HttpException(
+        'user.AlreadyExists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return this.userRepository.save(user);
+  }
+
+  findOne(id: number) {
+    if (!id) {
+      return null;
+    }
+    return this.userRepository.findOne(id);
+  }
+
+  find(email: string) {
+    return this.userRepository.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.userRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.userRepository.remove(user);
+  }
+}
